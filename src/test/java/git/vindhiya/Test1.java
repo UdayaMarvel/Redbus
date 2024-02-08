c57f9eb7fabd1cc9967d7866084cf85579b05f8e
vindhiyav <vindhiyajai24@gamil.com>
2024-02-08 22:04:03 +0530

    updated new Test1 class git.vindhiya pkg

--- src/test/java/git/vindhiya/Test1.java
+++ src/test/java/git/vindhiya/Test1.java
@@ -1,5 +1,22 @@
 package git.vindhiya;
 
 public class Test1 {
+	public static void main(String[] args) {
+		int n=898756;
+		int i,j=0;
+		
+		while(n>0) {
+		i=n%10;
+		j=(j*10)+i;
+		n=n/10;
+		}
+		if(n==j) {
+			System.out.println("palindrom"+j);
+		}
+		else {
+			System.out.println("not a polindrom"+j);
+			
+		}
+		  	
 
-}
+	}}
