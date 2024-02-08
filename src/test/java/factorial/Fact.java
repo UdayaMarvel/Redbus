6a5ab218ea1efc010cd864d90d1d301ddab1f093
Vickiejay18 <vickiejay1822@gmail.com>
2024-02-08 22:25:08 +0530

    Reverse factorial

---
+++ src/test/java/factorial/Fact.java
@@ -0,0 +1,23 @@
+package factorial;
+
+public class Fact {
+	
+	
+	public static void main(String[] args) {
+		
+		 int fac = 120;
+		 
+		for (int i = 5; i >1; i--) {
+			System.out.println(fac);
+			fac = fac/i;
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+
+}
