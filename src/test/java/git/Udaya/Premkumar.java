d14a725c34effc763842746cbfbd329c400c1655
premkumarsath <premsathya1212@gmail.com>
2024-02-08 19:04:48 +0530

    completed my task

---
+++ src/test/java/git/Udaya/Premkumar.java
@@ -0,0 +1,34 @@
+package git.Udaya;
+
+public class Premkumar {
+	public  Premkumar () { 
+		this(false);
+		System.out.println("integer Saravana");
+	}
+public  Premkumar (int id) { 
+	//this(false);
+	System.out.println("integer Saravana");
+	
+}
+public  Premkumar (String name) { 
+	this(123.45f);
+	System.out.println(" String Saravana");
+	
+}
+public  Premkumar (float value) { 
+ 
+	System.out.println("Float Saravana");
+	
+}
+public  Premkumar  (Boolean condition) { 
+	 this("123");
+	System.out.println("Boolean Saravana");
+	
+}
+public static void main(String[]args) {
+	Premkumar a = new Premkumar();
+	 	
+	 	}
+}
+	
+
