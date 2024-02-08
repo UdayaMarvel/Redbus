c83997063b56bbeacca6a1bacac4fce7166d0986
YuvashreeDayaram <yuvashreedayaram0612@gamil.com>
2024-02-08 18:56:06 +0530

    GIT

---
+++ src/test/java/git/Udaya/yuvashreeGIT.java
@@ -0,0 +1,12 @@
+package git.Udaya;
+
+public class yuvashreeGIT {
+	public static void main (String[]args) {
+	String z = "yuvashree";
+	 String rev = "";
+	 for(int i=z.length()-1;i>=0;i--) {
+	 char d=z.charAt(i);
+	 rev=rev+d;
+	 }
+	 System.out.println(rev);
+}}
