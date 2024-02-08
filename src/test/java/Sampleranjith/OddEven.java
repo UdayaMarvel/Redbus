465254c6353a5e8198003e67aa6a96770f095482
Ranjithsyg <ranjithsyg@gmail.com>
2024-02-08 18:54:18 +0530

    Odd or Even program

---
+++ src/test/java/Sampleranjith/OddEven.java
@@ -0,0 +1,22 @@
+package Sampleranjith;
+
+public class OddEven {		
+	public static void main(String[] args) {
+		for(int number=1; number<=100; number++)
+		{
+			if(number%2==0)
+			{
+				System.out.println("Even number:"+number);
+			
+			}
+			else if(number%2==1)
+			{
+				System.out.println("Odd number"+number);
+			}
+			
+		}
+	}
+
+	}
+
+
