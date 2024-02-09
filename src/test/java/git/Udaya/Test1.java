649f822851b01d48c95b1a45b22eddd2fafcaf7f
snehamagi <sneharamu1922@gmail.com>
2024-02-08 22:39:20 -0700

    updated class Test1 git.Udaya pkg

--- src/test/java/git/Udaya/Test1.java
+++ src/test/java/git/Udaya/Test1.java
@@ -1,7 +1,21 @@
 package git.Udaya;
 
 public class Test1 {
-public static void main(String[] args) {
-	System.out.println("New Update by Santos");
-}
+	public static void main(String[] args) {
+		 int n=153;
+		 int a,b=0;
+		 int i=n;
+		 while(n>0) {
+			 a=n%10;
+			 b=(a*a*a)+b;//b=(b*10)+a;
+			 n=n/10; 
+		 }
+		 if(i==b) {
+			 System.out.println("Amstrong");
+		 }else
+		 {
+			 System.out.println("Not a Amstrong");
+		 }
+	}
+
 }
